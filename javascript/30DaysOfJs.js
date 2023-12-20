98e7f51d792e166b9ca908a0d0482155efb402b5
Julio Orellana <coppro.cman@proton.me>
2023-12-20 17:07:38 +0100

    Solve 2726. Calculator with Method Chaining

--- javascript/30DaysOfJs.js
+++ javascript/30DaysOfJs.js
@@ -527,3 +527,74 @@ ArrayWrapper.prototype.toString = function() {
     return JSON.stringify(this.nums)
 }
 // ---------------------------------------------------------------
+
+// 2726. Calculator with Method Chaining
+class Calculator {
+    
+  /** 
+   * @param {number} value
+   */
+  constructor(value) {
+      this.value = value;
+  }
+  
+  /** 
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value){
+      this.value += value
+      return this;
+  }
+  
+  /** 
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value){
+    this.value -= value
+    return this;
+  }
+  
+  /** 
+   * @param {number} value
+   * @return {Calculator}
+   */  
+  multiply(value) {
+    this.value *= value
+    return this;
+  }
+  
+  /** 
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+    this.value /= value
+    return this;
+  }
+  
+  /** 
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {
+    this.value **= value;
+    return this;
+  }
+  
+  /** 
+   * @return {number}
+   */
+  getResult() {
+      return this.value
+  }
+}
+
+
+const newClalc = new Calculator(2).multiply(5).power(4)
+
+console.log(newClalc)
